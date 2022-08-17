2b91561b7cd29919a4349f198a3aac53a556b3fe
B-Oz <bettysoftwareng@gmail.com>
2022-08-17 23:20:43 +0300

    task1

---
+++ task1.js
@@ -0,0 +1,12 @@
+
+//? Write a program that asks the user for a number and prints the sum of the numbers up to the entered number
+
+
+
+let n = +prompt("Enter a number:");
+
+let sum = 0;
+for ( let i = 1 ; i <= n ; i++){
+    sum += i;
+
+}
