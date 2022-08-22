654639af90070295c2df777c72597d240570969d
B-Oz <bettysoftwareng@gmail.com>
2022-08-22 15:42:27 +0300

    fibonaccı

---
+++ fibonaccı.js
@@ -0,0 +1,7 @@
+//? FIBONACCI terms: 0, 1, 1, 2, 3, 5, 8, 13, 21, ...
+//* Entered n. Calculate the sum of the Fibonacci numbers up to the term and encode the function that prints it out with dongles.
+
+//*Sample outputs.
+fibonacci(4) = 3
+fibonacci(1) = 1
+fibonacci(8) = 21
